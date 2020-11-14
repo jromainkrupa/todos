94336a8fe0413f75dbdb7adbe97ed0add24a9bc8
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-11-14 09:56:56 +0100

    refactor test to use helper

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'User visits homepage' do
   scenario 'sucessfully' do
     visit '/'
-    expect(page).to have_css 'h1', text: 'Todos'
+
+    expect(page).to have_css('h1', text: 'Todos')
   end
 end
