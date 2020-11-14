5aa4ae91632378424bbbd0339b8d07b92fb4b403
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-11-14 08:57:30 +0100

    User model creation current_user refacto

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
   before_action :authenticate
   def index
-    @todos = Todo.where(email: current_email)
+    @todos = current_user.todos
   end
 
   def new
@@ -9,17 +9,13 @@ class TodosController < ApplicationController
   end
 
   def create
-    @todo = Todo.new(todo_params.merge(email: current_email))
-    if @todo.save
-      redirect_to todos_path
-    else
-      render :new
-    end
+    current_user.todos.create(todo_params)
+    redirect_to todos_path
   end
 
   private
 
   def todo_params
-    params.require(:todo).permit(:title, current_email)
+    params.require(:todo).permit(:title)
   end
 end
